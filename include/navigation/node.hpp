bb9b076ddaaea4ddbe072b04ff081cf85ba7e940
Kacper Kołodziej <kacper@kolodziej.in>
2015-02-16 18:49:25 +0100

    added NodeType, City and modified Node to use shared_ptr<Road>

--- include/navigation/node.hpp
+++ include/navigation/node.hpp
@@ -1,18 +1,21 @@
 #ifndef SPEDITOR_NAVIGATION_NODE_HPP
 #define SPEDITOR_NAVIGATION_NODE_HPP
+#include "node_type.hpp"
 #include "road.hpp"
+
 #include <vector>
+#include <memory>
 
 namespace speditor { namespace navigation {
 
 class Node
 {
 public:
-	virtual constexpr NodeType type();
-	void addRoad(Road);
+	virtual constexpr NodeType type() = 0;
+	void addRoad(std::shared_ptr<Road>);
 
 private:
-	std::vector<Road> roads_;
+	std::vector<std::shared_ptr<Road>> roads_;
 };
 
 } }
