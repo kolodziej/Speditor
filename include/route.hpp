9694dad3d2a0ae60b7c63e74e9d9b3cc14c792ed
Kacper Kołodziej <kacper@kolodziej.in>
2015-02-19 12:26:47 +0100

    modified naming

--- include/route.hpp
+++ include/route.hpp
@@ -14,14 +14,14 @@ class Route
 public:
 	Route(std::shared_ptr<Node>, std::shared_ptr<Node> = nullptr);
 
-	std::vector<std::shared_ptr<Node>> getMiddlePoints();
-	void addMiddlePoint(std::shared_ptr<Node>);
-	bool removeMiddlePoint(std::shared_ptr<Node>);
+	std::vector<std::shared_ptr<Node>> getAllNodes();
+	void addMiddleNode(std::shared_ptr<Node>);
+	bool removeMiddleNode(std::shared_ptr<Node>);
 
 private:
-	std::shared_ptr<Node> start_point_;
-	std::shared_ptr<Node> end_point_;
-	std::vector<std::shared_ptr<Node>> middle_points_;
+	std::shared_ptr<Node> start_node_;
+	std::shared_ptr<Node> end_node_;
+	std::vector<std::shared_ptr<Node>> middle_nodes_;
 	std::vector<std::shared_ptr<Road>> roads_;
 
 	friend class Map;
