c575eca5721e942bbf059ef220a2a2a8f08671ed
Kacper Kołodziej <kacper@kolodziej.in>
2015-04-14 23:48:16 +0200

    first modifications of coding style

--- include/clock.hpp
+++ include/clock.hpp
@@ -12,24 +12,24 @@ class Timepoint;
 
 class Clock
 {
-public:
-	Clock(unsigned int = 1000);
-	Timepoint timepoint();
-	void updateTime();
-	void reset();
+ public:
+  Clock(unsigned int = 1000);
+  Timepoint timepoint();
+  void updateTime();
+  void reset();
 
-	void run();
-	void wait();
+  void run();
+  void wait();
 
-private:
-	std::atomic_bool thread_running_;
-	std::thread clock_thread_;
-	std::mutex clock_mutex_;
-	const unsigned int minute_duration_;
-	long long time_;
+ private:
+  std::atomic_bool thread_running_;
+  std::thread clock_thread_;
+  std::mutex clock_mutex_;
+  const unsigned int minute_duration_;
+  long long time_;
 
-	std::chrono::steady_clock clock_;
-	std::chrono::time_point<std::chrono::steady_clock> start_;
+  std::chrono::steady_clock clock_;
+  std::chrono::time_point<std::chrono::steady_clock> start_;
 };
 
 }
--- include/timepoint.hpp
+++ include/timepoint.hpp
@@ -9,43 +9,43 @@ class Duration;
 class Timepoint
 {
 public:
-	Timepoint() :
-		time_{-1}
-	{}
-	Timepoint(long long);
-	Timepoint(short hour, short minute, int day = 1, int week = 1);
+  Timepoint() :
+    time_{-1}
+  {}
+  Timepoint(long long);
+  Timepoint(short hour, short minute, int day = 1, int week = 1);
 
-	void set(long long);
-	void set(short hour, short minute, int day = 1, int week = 1);
-	long long get() const;
+  void set(long long);
+  void set(short hour, short minute, int day = 1, int week = 1);
+  long long get() const;
 
-	short minute() const;
-	short hour() const;
-	int day() const;
-	short dayOfWeek() const;
-	int week() const;
+  short minute() const;
+  short hour() const;
+  int day() const;
+  short dayOfWeek() const;
+  int week() const;
 
-	Timepoint operator+(Duration) const;
-	Timepoint operator-(Duration) const;
-	Timepoint& operator+=(Duration);
-	Timepoint& operator-=(Duration);
+  Timepoint operator+(Duration) const;
+  Timepoint operator-(Duration) const;
+  Timepoint& operator+=(Duration);
+  Timepoint& operator-=(Duration);
 
-	Timepoint& operator++();
-	Timepoint operator++(int);
-	Timepoint& operator--();
-	Timepoint operator--(int);
+  Timepoint& operator++();
+  Timepoint operator++(int);
+  Timepoint& operator--();
+  Timepoint operator--(int);
 
-	bool operator>(Timepoint) const;
-	bool operator>=(Timepoint) const;
-	bool operator<(Timepoint) const;
-	bool operator<=(Timepoint) const;
-	bool operator==(Timepoint) const;
-	bool operator!=(Timepoint) const;
+  bool operator>(Timepoint) const;
+  bool operator>=(Timepoint) const;
+  bool operator<(Timepoint) const;
+  bool operator<=(Timepoint) const;
+  bool operator==(Timepoint) const;
+  bool operator!=(Timepoint) const;
 
-	operator bool() const;
-	
+  operator bool() const;
+  
 private:
-	long long time_;
+  long long time_;
 
 };
 
